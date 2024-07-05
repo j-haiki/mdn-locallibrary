7f92910a405f5da9ad2bad6d6bb03419ffd27e73
j-haiki <108611758+j-haiki@users.noreply.github.com>
2024-07-05 05:32:12 +0300

    Add author list and genre list

--- models/author.js
+++ models/author.js
@@ -21,4 +21,12 @@ AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
 });
 
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth ? this.date_of_birth.toLocaleDateString() : "";
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death ? this.date_of_death.toLocaleDateString() : "";
+});
+
 module.exports = mongoose.model("Author", AuthorSchema);
